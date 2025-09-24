6e7835c4c19ff8da995c53aee78ae8cf1eb5da20
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-09-24 17:01:29 +0500

    change: logo to real logo
    Signed-off-by: Umarov Ismoiljon <hs.umarov21@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,16 +23,16 @@ android {
         applicationId = "uz.umarov.fcneftchi"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
